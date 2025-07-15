c4acc872428662ccdadc24ce0fffde38d34306b7
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-07-15 19:36:39 +0900

    first commit

---
+++ src/main/java/com/dgsw/heckathon/HeckathonApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.heckathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeckathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeckathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/heckathon/HeckathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.heckathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeckathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
